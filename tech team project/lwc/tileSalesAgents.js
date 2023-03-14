a6cf0ab4bce692fb8289874bd2ea44800ee91921
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-03-14 18:23:19 +0300

    Create tileSalesAgents.js

---
+++ tech team project/lwc/tileSalesAgents.js
@@ -0,0 +1,34 @@
+import { LightningElement} from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getSADetails from "@salesforce/apex/SADetails.getSADetails";
+
+class TileSalesAgents extends NavigationMixin(LightningElement) {
+    searchTerm;
+    agents;
+    errors;
+
+    filterSA(event){
+        this.searchTerm = event.target.value;
+
+        getSADetails({name:this.searchTerm}).then(result => {
+            this.agents=result;
+            this.errors=undefined;
+        }).catch(error => {
+            this.agents=undefined;
+            this.errors=error;
+        });
+    }
+
+    goToRecordPage(event){
+        var recId = event.target.name;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: recId,
+                objectApiName: 'SA_Details__c',
+                actionName: 'view',
+            })
+    }
+}
+
+export default TileSalesAgents;
