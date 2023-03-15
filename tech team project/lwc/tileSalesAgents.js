088ab19e0aece8a786224b5d71e06d7e47ceaa92
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-03-16 00:31:52 +0300

    Update tileSalesAgents.js

--- tech team project/lwc/tileSalesAgents.js
+++ tech team project/lwc/tileSalesAgents.js
@@ -1,15 +1,17 @@
-import { LightningElement} from 'lwc';
+import { LightningElement, wire} from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import getSADetails from "@salesforce/apex/SADetails.getSADetails";
 
 class TileSalesAgents extends NavigationMixin(LightningElement) {
-    searchTerm;
-    agents;
+    searchTerm="";
     errors;
 
     filterSA(event){
         this.searchTerm = event.target.value;
+    }
+    @wire(getSADetails, {name:"$searchTerm"}) agents;
 
+  /*  connectedCallback(){
         getSADetails({name:this.searchTerm}).then(result => {
             this.agents=result;
             this.errors=undefined;
@@ -17,7 +19,7 @@ class TileSalesAgents extends NavigationMixin(LightningElement) {
             this.agents=undefined;
             this.errors=error;
         });
-    }
+    } */
 
     goToRecordPage(event){
         var recId = event.target.name;
