c6ec8bc4fbe1ba429edf073490125dcc29897c7c
Osman <96185425+smnryn999@users.noreply.github.com>
2024-04-22 21:01:27 +0300

    Update listTop5.js

--- movies project/listTop5.js
+++ movies project/listTop5.js
@@ -1,10 +1,34 @@
 import { LightningElement, track, wire } from 'lwc';
 import getSeries from '@salesforce/apex/GetWorksForLWC.getSeries';
 import getMovies from '@salesforce/apex/GetWorksForLWC.getMovies';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class ListMovies extends LightningElement {
+export default class ListMovies extends NavigationMixin(LightningElement) {
   
     @wire(getSeries) series;
     @wire(getMovies) movies;
 
+    goToTVShow(event){
+        const recId = event.target.name;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: recId,
+                objectApiName: 'Series__c',
+                actionName: 'view',
+            },
+        });
+    }
+
+    goToMovie(event){
+        const recId = event.target.name;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: recId,
+                objectApiName: 'Movies__c',
+                actionName: 'view',
+            },
+        });
+    }
 }
