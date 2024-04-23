a01718a1df910119be8867ea6940a26bb1e6b4fb
Osman <96185425+smnryn999@users.noreply.github.com>
2024-04-23 18:11:00 +0300

    Update homePage.js

--- movies project/homePage.js
+++ movies project/homePage.js
@@ -1,21 +1,83 @@
 import { LightningElement } from 'lwc';
 import insertTheMovie from "@salesforce/apex/GetMovies.insertTheMovie";
+import insertTheSeries from "@salesforce/apex/GetMovies.insertTheSeries";
+import insertBook from "@salesforce/apex/GetBooks.getBook";
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class HomePage extends LightningElement {
 
     movieName = "";
+    showName = "";
+    bookName = "";
+    authorName = "";
+
+    // Movie
     getMovieName(event){
         this.movieName = event.target.value;
     }
 
     saveMovie(){
-        alert(this.movieName);
+        console.log(this.movieName);
         insertTheMovie({mName:this.movieName})
         .then(()=>{
             const event = new ShowToastEvent({
                 title: 'Success',
                 message: this.movieName + ' has been created successfully.',
+                variant: "success"
+            });
+            this.dispatchEvent(event);
+        })
+        .catch(()=>{
+            const event = new ShowToastEvent({
+                title: 'Error',
+                message: 'There is a problem.',
+                variant: "error"
+            });
+            this.dispatchEvent(event);
+        });
+    }
+
+    // TV Show
+    getTVShowName(event){
+        this.showName = event.target.value;
+    }
+
+    saveTVShow(){
+        console.log(this.showName);
+        insertTheSeries({sName:this.showName})
+        .then(()=>{
+            const event = new ShowToastEvent({
+                title: 'Success',
+                message: this.showName + ' has been created successfully.',
+                variant: "success"
+            });
+            this.dispatchEvent(event);
+        })
+        .catch(()=>{
+            const event = new ShowToastEvent({
+                title: 'Error',
+                message: 'There is a problem.',
+                variant: "error"
+            });
+            this.dispatchEvent(event);
+        });
+    }
+
+    // Book
+    getBookName(event){
+        this.bookName = event.target.value;
+    }
+    getAuthorName(event){
+        this.authorName = event.target.value;
+    }
+
+    saveBook(){
+        console.log(this.bookName);
+        insertBook({bookName: this.bookName, authorName: this.authorName})
+        .then(()=>{
+            const event = new ShowToastEvent({
+                title: 'Success',
+                message: this.bookName + ' has been created successfully.',
                 variant: "success"
             });
             this.dispatchEvent(event);
